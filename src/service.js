32cd4259dbbb4c05acecf29913969f2d23f0e356
zeeberry <zainabebrahimi@gmail.com>
2018-03-04 17:56:20 -0500

    add request handler to service

--- src/service.js
+++ src/service.js
@@ -1,9 +1,19 @@
+export const service = {
+  signup
+};
+
 function signup(user) {
   const requestOpts = {
     method: 'POST',
-    body: JSON.stringify(user);
+    body: JSON.stringify(user)
   };
 
-  return fetch('user/signup', requestOpts).then(handleResponse);
+  return fetch('user/signup', requestOpts).then((response) => {
+    if (!response.ok) {
+      return Promise.reject(response.statusText);
+    }
+  
+    return response.json();
+  });
 }
 
