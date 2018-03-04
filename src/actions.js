8009df10a72a026735dd07e4a6467b4a692e255b
zeeberry <zainabebrahimi@gmail.com>
2018-03-04 15:42:00 -0500

    adding actions and action creators

--- src/actions.js
+++ src/actions.js
@@ -5,6 +5,29 @@ export const SIGNUP_USER_SUCCESS = 'SIGNUP_USER_SUCCESS';
 export const SIGNUP_USER_FAILURE = 'SIGNUP_USER_FAILURE';
 
 //Action Creators
-export function signupUser(userId) {
-  return { type: SIGNUP_USER, userId };
+export function signupUserRequest(userId) {
+  return { 
+    type: SIGNUP_USER_REQUEST,
+    payload: {
+      userId: userId
+    }
+  };
+}
+
+export function signupUserSuccess(userId, response) {
+  return { 
+    type: SIGNUP_USER_SUCCESS,
+    payload: {
+      userId: userId,
+      response: response
+    }
+  };
+}
+
+export function signupUserFailure() {
+  return { 
+    type: SIGNUP_USER_FAILURE,
+    payload: new Error(),
+    error: true
+  };
 }
