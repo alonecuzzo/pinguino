7880c7a53559b188507be286d9fda7d7aebec96f
zeeberry <zainabebrahimi@gmail.com>
2018-03-04 15:41:34 -0500

    form fields to controlled components

--- src/SignupForm.js
+++ src/SignupForm.js
@@ -5,27 +5,32 @@ class SignupForm extends React.Component {
     super(props);
 
     this.state = {
-      user: {
-        name: '',
-        email: '',
-        zipcode: ''
-      },
-
+      username: '',
+      email: '',
+      zipcode: ''
     };
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
   }
-  
+
+  handleSubmit(event) {
+    event.preventDefault();
+  }
+
+  handleChange(event) {
+    const { name, value } = event.target;
+    this.setState({ [name]: value });
+  }
+
   render() {
     return (
-      <div class="signupForm">
-        <div class="signupForm__fields">
-        <input type="text" placeholder="Name"/>  
-        <input type="text" placeholder="Email"/>
-        <input type="text" pattern="[0-9]{5}"placeholder="Zipcode"/>  
-        </div>
-        <div class="signupForm__button">
-          <button class="signupForm__button--primary">Signup</button>
-        </div>
-      </div>
+      <form onSubmit={this.handleSubmit}>
+        <input type="text" placeholder="Name" name="username" value={this.state.username} onChange={this.handleChange} />
+        <input type="text" placeholder="Email" name="email" value={this.state.email} onChange={this.handleChange} />
+        <input type="text" pattern="[0-9]{5}"placeholder="Zipcode" name="zipcode" value={this.state.zipcode} onChange={this.handleChange} />
+        <button className="signupForm__button--primary">Signup</button>
+      </form>
     );
   }
 }
