eeb24bf58cc0737f9396dfbec19fff63800723bd
zeeberry <zainabebrahimi@gmail.com>
2018-03-04 17:53:16 -0500

    import createStore

--- src/App.js
+++ src/App.js
@@ -1,13 +1,12 @@
 import React, { Component } from 'react';
+import { createStore } from 'react-redux';
 import SignupForm from './SignupForm';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <SignupForm />
-      </div>
+      <SignupForm />
     );
   }
 }
