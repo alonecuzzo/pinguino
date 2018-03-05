cf40ef83be0d0ae2dae1a7545c939fed736f33eb
zeeberry <zainabebrahimi@gmail.com>
2018-03-04 20:37:05 -0500

    adding redux thunk and applying to store

--- src/index.js
+++ src/index.js
@@ -2,14 +2,18 @@ import React from 'react';
 import App from './App';
 import './index.css';
 import { Provider } from 'react-redux';
-import { createStore } from 'redux';
+import thunkMiddleware from 'redux-thunk';
+import { createStore, applyMiddleware } from 'redux';
 import { render } from 'react-dom';
 import { signup } from './reducer';
 import { initializeMockBackend } from './mockBackend';
 
 initializeMockBackend();
 
-let store = createStore(signup);
+let store = createStore(
+    signup,
+    applyMiddleware(thunkMiddleware)
+);
 
 render(
     <Provider store={ store }>
