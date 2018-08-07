bdebdaff1987c81013b7fdca2c07f2806e37bccc
zeeberry <zainabebrahimi@gmail.com>
2018-08-07 18:12:45 -0400

    adding referrer info

--- src/App.js
+++ src/App.js
@@ -17,7 +17,7 @@ class App extends Component {
           <Route exact path="/" component={UserCreationForm} />
           <Route path="/users" component={UserList} />
           <footer className="pinguino--footer">
-            <div>Icons made by <a href="https://www.flaticon.com/authors/freepik" title="Penguin">Penguin</a> from <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a> is licensed by <a href="http://creativecommons.org/licenses/by/3.0/" title="Creative Commons BY 3.0" target="_blank">CC 3.0 BY</a></div>
+            <div>Icons made by <a href="https://www.flaticon.com/authors/freepik" title="Penguin">Penguin</a> from <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a> is licensed by <a href="http://creativecommons.org/licenses/by/3.0/" title="Creative Commons BY 3.0" target="_blank" rel="noopener noreferrer">CC 3.0 BY</a></div>
           </footer>
         </div>
       </Router>
