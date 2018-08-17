43b35cd9bae893bb1a71ea96a9e1bf368d218c55
zeeberry <zainabebrahimi@gmail.com>
2018-08-17 10:35:23 -0400

    dynamically display initials

--- src/UserItem.js
+++ src/UserItem.js
@@ -5,13 +5,20 @@ import './UserItem.css';
 
 class UserItem extends React.Component {
 
+  getInitials(name) {
+    if (name) {
+      return name.slice(0,2);
+    }
+  }
+
   render() {
     const { user } = this.props;
+    const initials = this.getInitials(user.name);
     return (
       <div className="user-item">
         <div className="user-item__content">
           <div className="user-item__left">
-            <span className="user-item__avatar">ZE</span>
+            <span className="user-item__avatar">{initials}</span>
           </div>
           <div className="user-item__right">
             <div className="user-item__name">{user.name}</div>
