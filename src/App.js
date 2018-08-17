f1f1e71485a9bd85614772b3853042a41a10587d
zeeberry <zainabebrahimi@gmail.com>
2018-08-17 09:59:03 -0400

    removing footer and adjusting size of map

--- src/App.js
+++ src/App.js
@@ -17,9 +17,6 @@ class App extends Component {
           <Route exact path="/" component={UserCreationForm} />
           <Route path="/users" component={UserList} />
           <Route path="/usermap" component={UserMap} />
-          <footer className="pinguino--footer">
-            <div>Icons made by <a href="https://www.flaticon.com/authors/freepik" title="Penguin">Penguin</a> from <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a> is licensed by <a href="http://creativecommons.org/licenses/by/3.0/" title="Creative Commons BY 3.0" target="_blank" rel="noopener noreferrer">CC 3.0 BY</a></div>
-          </footer>
         </div>
       </Router>
     );
